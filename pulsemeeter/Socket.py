c89e8db52b87616e753e8d87a8ab5e0688a02691
dacid44 <dacid44@gmail.com>
2022-02-06 00:27:15 -0600

    added boilerplate threading and message passing code

--- pulsemeeter/Socket.py
+++ pulsemeeter/Socket.py
@@ -1,9 +1,11 @@
-import socket 
+import socket
 import sys
 import os
+import signal
 import threading
-from queue import Queue
+from queue import SimpleQueue
 from .settings import SOCK_FILE
+
 
 class Server:
     def __init__(self, audio_server):
@@ -11,14 +13,21 @@ class Server:
         # audio server can be pulse or pipe, so just use a generic name
         self.audio_server = audio_server
 
-        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        # the socket only needs to be seen by the listener thread
+        # self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-        # list of messages to deliver to listening clients
-        self.msg_queue = []
+        #    # list of messages to deliver to listening clients
+        #    self.msg_queue = []
+        #
+        #    # list of clients
+        #    self.client_list = []
+        #    self.client_command_thread_list = []
 
-        # list of clients
-        self.client_list = []
-        self.client_command_thread_list = []
+        self.exit_flag = False
+        self.command_queue = SimpleQueue()
+        self.event_queues = {}
+        self.client_handler_threads = {}
+        self.client_handler_connections = {}
 
         # delete socket file if exists
         try:
@@ -27,43 +36,109 @@ class Server:
             if os.path.exists(SOCK_FILE):
                 raise
 
-        self.sock.bind(SOCK_FILE)
-        self.sock.listen(1)
-        self.querry_clients()
+        # Start listener thread
+        self.listener_thread = threading.Thread(target=self.query_clients)
+        self.listener_thread.start()
 
+        # Register signal handlers
+        signal.signal(signal.SIGINT, self.handle_exit_signal)
+        signal.signal(signal.SIGTERM, self.handle_exit_signal)
+
+        # Listen for commands
+        while True:
+            message = self.command_queue.get()
+            # if the message is None, exit. Otherwise, pass it to handle_command().
+            # The string matching could probably be replaced with an enum if it's too slow
+            if message[0] == 'exit':
+                break
+            elif message[0] == 'client_handler_exit':
+                if message[1] in self.event_queues:
+                    del self.event_queues[message[1]]
+                del self.client_handler_connections[message[1]]
+                self.client_handler_threads.pop(message[1]).join()
+            elif message[0] == 'command':
+                # TODO: as mentioned in handle_command(), it probably needs a rework. This is for boilerplate.
+                ret_message = self.handle_command(message)
+                self.event_queues[message[1]].put(('return', ret_message))
+
+        # TODO: maybe here would be the spot to sent an event signifying that the daemon is shutting down
+        # TODO: if we do that, have those client handlers close by themselves instead of shutting down their connections
+        # And maybe wait a bit for that to happen
+
+        # Close connections and join threads
+        for conn in self.client_handler_connections.values():
+            conn.shutdown()
+
+        # Not sure if joining the client handler threads is actually necessary since we're stopping anyway and the
+        # client handler threads are in daemon mode
+        for thread in self.client_handler_threads:
+            thread.join()
+
+        # Set the exit flag and wait for the listener thread to timeout
+        self.exit_flag = True
+        self.listener_thread.join()
+
+        # Call any code to clean up virtual devices or similar
+        self.close_server()
+
+    # function to register as a signal handler to gracefully exit
+    def handle_exit_signal(self, signum, frame):
+        self.command_queue.put(('exit',))
+
+    # TODO: if the daemon should clean up its virtual devices on exit, do that here
+    def close_server(self):
+        pass
 
     # this function handles the connection requests
-    def querry_clients(self): 
-
+    def query_clients(self):
         # loop to get new connections
-        while True:
-            # Wait for a connection
-            print('waiting for a connection')
-            connection, client_address = self.sock.accept()
-            print('client connected ', client_address)
-            
-            # create a thread for that client and store
-            thread = threading.Thread(target=self.listen_to_client, args=(connection,))
-            thread.start()
-            # self.client_command_thread_list.append(thread)
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+            s.settimeout(5)
+            s.bind(SOCK_FILE)
+            s.listen()
+            i = 0
+            while True:
+                try:
+                    # Wait for a connection
+                    print('waiting for a connection')
+                    conn, addr = s.accept()
+                    print('client connected ', addr)
+
+                    # Create a thread for the client
+                    event_queue = SimpleQueue()
+                    thread = threading.Thread(target=self.listen_to_client, args=(conn, event_queue, i), daemon=True)
+                    thread.start()
+                    self.client_handler_threads[i] = thread
+                    self.client_handler_connections[i] = conn
+                    self.event_queues[i] = event_queue
+                except socket.timeout:
+                    if self.exit_flag:
+                        break
 
     # get data stream and pass it into command handling function
-    def listen_to_client(self, connection):
-        while True:
-            try:
-                data = connection.recv(20)
-                if not data: raise
+    def listen_to_client(self, conn, event_queue, id):
+        with conn:
+            while True:
+                try:
+                    # TODO: rework to include the length as the first 4 bytes. Get the daemon working first though, then
+                    # TODO: work on compatibility.
+                    data = conn.recv(20)
+                    if not data: raise
 
-                print(data.decode())
-                ret_message = self.handle_command(data)
-                connection.sendall(ret_message)
-                if ret_message == False:
-                    raise
-            except:
-                print('client disconnect')
-                connection.close()
-                break
-
+                    print(data.decode())
+                    self.command_queue.put(('command', data))
+                    # TODO: If this handler is being used for sending events to clients, distinguish a return value from
+                    # TODO: an event
+                    ret_message = event_queue.get()
+                    conn.sendall(ret_message[1])
+                    if ret_message == False:
+                        raise
+                except Exception:  # Exception doesn't catch exit exceptions (a bare except clause does)
+                    print('client disconnect')
+                    conn.shutdown(socket.SHUT_RDWR)
+                    # Notify the main process that this client handler is closing, so it can free its resources
+                    self.command_queue.put(('client_handler_exit', id))
+                    break
 
     # needs rework
     def handle_command(self, data):
@@ -105,6 +180,7 @@ class Server:
 
         return b' '
 
+
 class Client:
     def __init__(self, command=None, is_listen=False):
 
@@ -115,7 +191,7 @@ class Client:
             self.sock.connect(SOCK_FILE)
         except socket.error as msg:
             print(msg)
-            sys.exit(1)   
+            sys.exit(1)
             if is_listen == True: self.listen()
 
     def send_command(self, command):
