7fba8d89922b87c1c89d9574531f766a559b4deb
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2023-02-19 08:48:37 -0300

    add validation exception

--- meexer/api/app_api.py
+++ meexer/api/app_api.py
@@ -3,6 +3,7 @@ from meexer.schemas import ipc_schema
 from meexer.schemas.ipc_schema import SubscriptionFlags as sflags
 from meexer.ipc.server import Server as ipc
 from meexer.model.app_model import AppModel
+from pydantic.error_wrappers import ValidationError
 
 
 @ipc.command('app_move', sflags.APP, save_config=False)
@@ -10,7 +11,12 @@ def app_move(req: requests.AppMove):
     '''
     Recives an AppMove request, changes the device an app is bound into
     '''
-    app_move_req = requests.AppMove(**req)
+
+    try:
+        app_move_req = requests.AppMove(**req)
+    except ValidationError:
+        return ipc_schema.StatusCode.INVALID, None
+
     app_model = AppModel.construct(app_move_req.app)
     app_model.change_device(app_move_req.device)
 
@@ -20,10 +26,14 @@ def app_volume(req: requests.AppVolume):
     '''
     Recives a volume reques
     '''
-    app_volume_req = requests.AppVolume(**req)
+
+    try:
+        app_volume_req = requests.AppVolume(**req)
+    except ValidationError:
+        return ipc_schema.StatusCode.INVALID, None
+
     app_model = AppModel.construct(app_volume_req.app)
     app_model.set_volume(app_volume_req.volume)
-    # pmctl.mute(mute.index.device_type, mute.index.device_id, status=mute.state)
 
 
 @ipc.command('app_list', sflags.APP, notify=False, save_config=False)
@@ -32,7 +42,11 @@ def app_list(req: requests.AppList):
     Recives an AppList request, returns a list of apps of a given type
     '''
 
-    app_list_req = requests.AppList(**req)
+    try:
+        app_list_req = requests.AppList(**req)
+    except ValidationError:
+        return ipc_schema.StatusCode.INVALID, None
+
     app_list = AppModel.list_apps(app_list_req.app_type)
 
     return ipc_schema.StatusCode.OK, app_list
