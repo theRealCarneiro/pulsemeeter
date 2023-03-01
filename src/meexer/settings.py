897f7ab2bcc1a252a19aee22219111ada1e6e9fe
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2023-03-01 10:38:42 -0300

    use XDG_RUNTIME_DIR for socket

--- src/meexer/settings.py
+++ src/meexer/settings.py
@@ -13,6 +13,7 @@ DEBUG = True
 USER = os.getenv('USER')
 HOME = os.getenv('HOME', os.getenv('USERPROFILE'))
 XDG_CONFIG_HOME = os.getenv('XDG_CONFIG_HOME', os.path.join(HOME, '.config'))
+XDG_RUNTIME_DIR = os.getenv('XDG_RUNTIME_DIR', '/tmp')
 CONFIG_DIR = os.path.join(XDG_CONFIG_HOME, 'pulsemeeter')
 CONFIG_FILE = os.path.join(CONFIG_DIR, 'config.json')
 
@@ -20,8 +21,8 @@ CONFIG_FILE = os.path.join(CONFIG_DIR, 'config.json')
 LISTENER_TIMEOUT = 2
 CLIENT_ID_LEN = 5
 REQUEST_SIZE_LEN = 5
-SOCK_FILE = f'/tmp/pulsemeeter.{USER}.sock'
-PIDFILE = f'/tmp/pulsemeeter.{USER}.pid'
+SOCK_FILE = os.path.join(XDG_RUNTIME_DIR, f'pulsemeeter.{USER}.sock')
+PIDFILE = os.path.join(XDG_RUNTIME_DIR, f'pulsemeeter.{USER}.pid')
 
 
 # logging formats
