1d688ff1c8835788cbdfe940955a8e126f1f145d
Gabriel Carneiro <gabriel.chaves.carneiro@gmail.com>
2025-06-06 14:57:59 -0300

    use babel in setup.py to manage translations

--- build_translations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for file in src/pulsemeeter/locale/*/; do
-	# echo $file
-	msgfmt ${file}/LC_MESSAGES/pulsemeeter.po -o ${file}/LC_MESSAGES/pulsemeeter.mo
-done
--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 import os
 from setuptools import setup
+from babel.messages import frontend as babel
+from setuptools.command.build_py import build_py as _build_py
+
+
+class build_with_translations(_build_py):
+    def run(self):
+        self.run_command("compile_catalog")
+        super().run()
 
 
 DATA_FILES = [('share/licenses/pulsemeeter/', ['LICENSE']), ]
@@ -10,4 +18,24 @@ for directory, _, filenames in os.walk('share'):
         files = [os.path.join(directory, filename) for filename in filenames]
         DATA_FILES.append((os.path.join('share', dest), files))
 
-setup(data_files=DATA_FILES)
+# package .mo files
+for directory, _, filenames in os.walk('locale'):
+    if filenames:
+        files = [os.path.join(directory, filename) for filename in filenames if '.mo' in filename]
+        DATA_FILES.append((os.path.join('share', directory), files))
+
+setup(
+    data_files=DATA_FILES,
+    cmdclass={
+        'build_py': build_with_translations,
+        'compile_catalog': babel.compile_catalog,
+        'extract_messages': babel.extract_messages,
+        'init_catalog': babel.init_catalog,
+        'update_catalog': babel.update_catalog
+    },
+    message_extractors={
+        'src': [
+            ('**/*.py', 'python', None),
+        ],
+    },
+)
