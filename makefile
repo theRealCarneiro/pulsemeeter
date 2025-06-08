f51a89ca48d4806526d1ff692c48bf26f2d73743
Gabriel Carneiro <gabriel.chaves.carneiro@gmail.com>
2025-06-08 16:23:45 -0300

    Add makefile to compile zip build

---
+++ makefile
@@ -0,0 +1,42 @@
+# Pulsemeeter ZIP install
+.PHONY: zip install uninstall
+
+BUILD_DIR=./buildzip
+DIST_DIR=${BUILD_DIR}/dist
+PKG_DIR=${BUILD_DIR}/pkg
+
+all: zip
+
+zip:
+	@echo Removing old build files
+	rm -rf ${BUILD_DIR}
+	@echo Building source
+	pip install . -t ${DIST_DIR}
+	@echo Moving data files to package
+	mkdir -p ${PKG_DIR}
+	mv ${DIST_DIR}/bin ${PKG_DIR}
+	mv ${DIST_DIR}/share ${PKG_DIR}
+	@echo Deleting unnecessary files
+	rm -rf ${DIST_DIR}/pydantic_core ${DIST_DIR}/include ${DIST_DIR}/*.dist-info
+	@echo Zipping package
+	python -m zipapp ${DIST_DIR} -m "pulsemeeter.main:main" -o \
+		${PKG_DIR}/bin/pulsemeeter -p '/usr/bin/env python3'
+
+install: zip
+	mkdir -p $(DESTDIR)$(PREFIX)/bin
+	install -Dm755 ${PKG_DIR}/bin/* $(DESTDIR)$(PREFIX)/bin
+	install -Dm644 LICENSE ${DESTDIR}${PREFIX}/share/licenses/pulsemeeter/LICENSE
+	install -Dm644 README.md ${DESTDIR}${PREFIX}/share/doc/pulsemeeter/README.md
+	cp -r ${PKG_DIR}/share ${DESTDIR}${PREFIX}/
+
+uninstall:
+	rm -rf $(DESTDIR)$(PREFIX)/bin/pulsemeeter \
+		$(DESTDIR)$(PREFIX)/bin/pmctl \
+		${DESTDIR}${PREFIX}/share/licenses/pulsemeeter \
+		${DESTDIR}${PREFIX}/share/doc/pulsemeeter \
+		${DESTDIR}${PREFIX}/share/applications/pulsemeeter.desktop \
+		${DESTDIR}${PREFIX}/share/locale/*/LC_MESSAGES/pulsemeeter.mo \
+		${DESTDIR}${PREFIX}/share/icons/hicolor/*/apps/Pulsemeeter.png \
+
+clean:
+	rm -rf ${BUILD_DIR} ./build
--- setup.py
+++ setup.py
@@ -1,41 +1,46 @@
 import os
-from setuptools import setup
 from babel.messages import frontend as babel
+from setuptools import setup
 from setuptools.command.build_py import build_py as _build_py
 
+IS_ZIP = os.environ.get("ZIP_BUILD") == "1"
 
-class build_with_translations(_build_py):
+
+def gather_data_files(source_dir, target_dir, pattern=''):
+    datafiles = []
+    for directory, _, filenames in os.walk(source_dir):
+        if filenames:
+            files = [os.path.join(directory, filename) for filename in filenames if pattern in filename]
+            datafiles.append((os.path.join(target_dir, directory), files))
+
+    return datafiles
+
+
+class BuildWithTranslations(_build_py):
     def run(self):
         self.run_command("compile_catalog")
 
-        # package .mo files
-        mo_files = []
-        for directory, _, filenames in os.walk('locale'):
-            if filenames:
-                files = [os.path.join(directory, filename) for filename in filenames if '.mo' in filename]
-                mo_files.append((os.path.join('share', directory), files))
-
+        mo_files = gather_data_files('locale', 'share', '.mo')
         self.distribution.data_files.extend(mo_files)
+
         super().run()
 
 
 DATA_FILES = [('share/licenses/pulsemeeter/', ['LICENSE']), ]
-
-for directory, _, filenames in os.walk('share'):
-    dest = directory[6:]
-    if filenames:
-        files = [os.path.join(directory, filename) for filename in filenames]
-        DATA_FILES.append((os.path.join('share', dest), files))
+DATA_FILES += gather_data_files('share', '')
 
 setup(
     data_files=DATA_FILES,
+
     cmdclass={
-        'build_py': build_with_translations,
+        'build_py': BuildWithTranslations,
+        # 'build_zip': BuildWithTranslations,
         'compile_catalog': babel.compile_catalog,
         'extract_messages': babel.extract_messages,
         'init_catalog': babel.init_catalog,
         'update_catalog': babel.update_catalog
     },
+
     message_extractors={
         'src': [
             ('**/*.py', 'python', None),
