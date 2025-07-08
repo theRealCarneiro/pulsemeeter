8493810b89157d74757f0149ed7a15d2aaa57bb5
Gabriel Carneiro <gabriel.chaves.carneiro@gmail.com>
2025-07-08 19:25:44 -0300

    add simple cli client

---
+++ src/pulsemeeter/clients/cli/cli_client.py
@@ -0,0 +1,79 @@
+import logging
+import gettext
+import argparse
+
+from pulsemeeter.settings import VERSION
+
+LOG = logging.getLogger('generic')
+_ = gettext.gettext
+
+DEVICE_TYPES = {
+    'device': ('hi', 'vi', 'a', 'b'),
+    'input': ('hi', 'vi'),
+    'output': ('a', 'b')
+}
+
+
+def make_device_type_parser(allowed_types, label="device"):
+    '''
+    Sets allowed types and return the correct parser
+    '''
+
+    def parser(device_type):
+        '''
+        Checks if a device type is valid
+        '''
+        if device_type in allowed_types:
+            return device_type
+
+        raise argparse.ArgumentTypeError(f"Invalid {label} type: {device_type}")
+
+    return parser
+
+
+def parse_device_id(device_id):
+    '''
+    Checks if the device type is a number gt 0
+    '''
+    if device_id.isdigit() and int(device_id) > 0:
+        return device_id
+
+    raise argparse.ArgumentTypeError(_("Invalid device id: %s") % device_id)
+
+
+def add_device_args(parser, device_class='device'):
+    '''
+    Inserts device type and id args into a parser
+    '''
+
+    device_parser = make_device_type_parser(DEVICE_TYPES[device_class], label=device_class)
+    parser.add_argument(f"{device_class}_type", type=device_parser)
+    parser.add_argument(f"{device_class}_id", type=parse_device_id)
+
+
+def parse_args():
+    LOG.debug('Parsing args...')
+    parser = argparse.ArgumentParser(description='Pulsemeeter')
+    subparser = parser.add_subparsers(dest='command')
+
+    subparser.add_parser('init', help='INIT')
+
+    volume_parser = subparser.add_parser('volume', help='Set the volume')
+    add_device_args(volume_parser)
+    volume_parser.add_argument('value', type=int)
+
+    mute_parser = subparser.add_parser('mute', help='Set the mute state')
+    add_device_args(mute_parser)
+    mute_parser.add_argument('value', type=bool, nargs='?', default=None)
+
+    primary_parser = subparser.add_parser('primary', help='Set the primary device')
+    add_device_args(primary_parser)
+
+    connect_parser = subparser.add_parser('connect', help='Set the connection state')
+    add_device_args(connect_parser, 'input')
+    add_device_args(connect_parser, 'output')
+    connect_parser.add_argument('value', type=bool, nargs='?', default=None)
+
+    parser.add_argument('-v', '--version', action='version', version=VERSION)
+
+    return parser.parse_args()
--- src/pulsemeeter/main.py
+++ src/pulsemeeter/main.py
@@ -2,86 +2,58 @@
 Entry point
 '''
 import logging
-# import time
-import sys
 
-# from pulsemeeter.api.app_api import ipc as app_routes
-# from pulsemeeter.api.device_api import ipc as device_routes
-# from pulsemeeter.api.server_api import ipc as server_routes
-# from pulsemeeter.api.pulse_events_api import task as pulse_events_task
-
-# from pulsemeeter.scripts import argparser
 from pulsemeeter.model.config_model import ConfigModel
 from pulsemeeter.clients.gtk.gtk_client import GtkClient
-# from pulsemeeter.ipc.server_async import Server
-# from pulsemeeter.ipc.client import Client
-from pulsemeeter.settings import VERSION
+from pulsemeeter.clients.cli.cli_client import parse_args
 
 LOG = logging.getLogger("generic")
 
 
+def start_gui():
+    app = GtkClient()
+    app.run()
+
+
+def init_devices():
+    ConfigModel.load_config()
+
+
+def mute(device_type, device_id, state):
+    config = ConfigModel.load_config()
+    config.device_manager.set_mute(device_type, device_id, state)
+
+
+def volume(device_type, device_id, value):
+    config = ConfigModel.load_config()
+    config.device_manager.set_volume(device_type, device_id, value)
+
+
+def primary(device_type, device_id):
+    config = ConfigModel.load_config()
+    config.device_manager.set_primary(device_type, device_id)
+
+
+def connect(input_type, input_id, output_type, output_id, state):
+    config = ConfigModel.load_config()
+    config.device_manager.set_connection(input_type, input_id, output_type, output_id, state)
+
+
 def main():
-    '''
-    Entry point and simple argparser
-    '''
 
-    # try:
-    #     server = Server()
-    #     isserver = True
-    #     server.register_blueprint(app_routes)
-    #     server.register_blueprint(device_routes)
-    #     server.register_blueprint(server_routes)
-    #     server.register_task(device_routes)
-    #
-    # except ConnectionAbortedError:
-    #     isserver = False
+    arg_map = {
+        None: start_gui,
+        'init': init_devices,
+        'mute': mute,
+        'volume': volume,
+        'primary': primary,
+        'connect': connect
+    }
 
-    # simple args parser
-    match sys.argv[1:]:
-
-        # no args: open window
-        case []:
-            # trayonly = False
-
-            # if isserver:
-            #     server.start_server(daemon=False)
-
-            app = GtkClient()
-            app.run()
-
-            # server.close_server()
-
-        # daemon: start only the server
-        # case ['daemon']:
-        #     if not isserver:
-        #         LOG.error('There\'s another server instance running')
-        #         return 1
-
-            # trayonly = True
-            # server.start_server(daemon=True)
-            # server.exit_signal()
-
-        # init: Just start devices and connections
-        case ['init']:
-            # server.init_audio()
-            ConfigModel.load_config()
-
-        case ['--version']:
-            print(VERSION)
-
-        # exit: close server, clients should close after they recive an exit signal
-        case ['exit']:
-            # if server:
-            #     LOG.error('No instance is running')
-            #     return 1
-
-            # TODO: close
-            LOG.info('Closing server, it may take a few seconds...')
-
-        # default: call cli interface
-        case _:
-            pass
-            # argparser.create_parser_args()
+    parsed = parse_args()
+    command = parsed.command
+    args = list(vars(parsed).values())[1:]
+    arg_map[command](*args)
 
     return 0
 
