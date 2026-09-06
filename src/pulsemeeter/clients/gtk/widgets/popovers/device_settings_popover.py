0fd60270c8efffe549ef8f055d395bb31566db3d
ZorudaRinku <stronger6@gmail.com>
2026-09-06 16:33:42 -0600

    Fix missing space

--- src/pulsemeeter/clients/gtk/widgets/popovers/device_settings_popover.py
+++ src/pulsemeeter/clients/gtk/widgets/popovers/device_settings_popover.py
@@ -30,7 +30,7 @@ class DeviceSettingsPopover(Gtk.Popover):
         self.external_widget = Gtk.CheckButton(label=_('Externally Managed'))
         self.external_widget.set_tooltip_text(
             _("Check this if another application or config file owns this device's lifecycle (creates and destroys it), "
-              "such as a PipeWire config or EasyEffects. Pulsemeeter will route and control the device but will not create it on"
+              "such as a PipeWire config or EasyEffects. Pulsemeeter will route and control the device but will not create it on "
               "startup or remove it on exit.")
         )
         self.port_selector = PortSelector()
